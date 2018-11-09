69cef6f8cecc1e3cac01598d6ca3b6bb746fd5b4
Rayturner677 <rturner@basecampcodingacademy.org>
2018-11-09 11:24:23 -0600

    new game working

--- javascript.js
+++ javascript.js
@@ -13,6 +13,11 @@ var LoginInput = document.getElementById("username-login");
 var invalid = document.getElementById("invalid-text");
 var subtractBtn1 = document.getElementById("minus-score-btn1");
 var subtractBtn2 = document.getElementById("minus-score-btn");
+var newGameBtn = document.querySelector(".start-game");
+var newGameDiv = document.querySelector(".new-game");
+var select1 = document.getElementById("select-1");
+var select2 = document.getElementById("select-2");
+
 var PAGE_DATA = {};
 
 function showRegister() {
@@ -85,9 +90,11 @@ function addScore() {
     score1.addEventListener("click", function() {
         count += 1;
         score1.innerText = count;
+        PAGE_DATA.points.push(Number(PAGE_DATA.player1));
         if (score1.innerText === "10") {
             score1.disabled = true;
             score2.disabled = true;
+            submitScore();
             restartPopup.style.display = "block";
             resetGame(score1, score2);
             count = 0;
@@ -97,9 +104,11 @@ function addScore() {
     score2.addEventListener("click", () => {
         score += 1;
         score2.innerText = score;
+        PAGE_DATA.points.push(Number(PAGE_DATA.player2));
         if (score2.innerText === "10") {
             score2.disabled = true;
             score1.disabled = true;
+            submitScore();
             restartPopup.style.display = "block";
             resetGame(score1, score2);
             score = 0;
@@ -114,6 +123,8 @@ function resetGame(score1, score2) {
     var homeScore2 = document.getElementById("home-score2");
 
     restartBtn.addEventListener("click", () => {
+        profile.style.display = "none";
+        newGameDiv.style.display = "block";
         homeScore1.innerText = score1.innerText;
         homeScore2.innerText = score2.innerText;
         score1.innerText = "0";
@@ -142,7 +153,8 @@ function fetchLoginInfo() {
                 mainContent.style.display = "none";
                 registerForm.style.display = "none";
                 loginDiv.style.display = "none";
-                profile.style.display = "block";
+                profile.style.display = "none";
+                newGameDiv.style.display = "block";
                 // console.log({
                 //     username: userName.value,
                 //     password: passWord.value
@@ -153,6 +165,12 @@ function fetchLoginInfo() {
 
                 welcome.innerHTML = LoginInput.value;
 
+                newGameBtn.addEventListener("click", () => {
+                    getId();
+                    fetchNewGame();
+                    profile.style.display = "block";
+                    newGameDiv.style.display = "none";
+                });
                 // RAY TAKE THIS OUT LATER
                 fetchUser();
             }
@@ -176,7 +194,7 @@ function loadUsers() {
 
     var html = "";
     usernames.forEach(user => {
-        html += "<option>" + user.username + "</option>";
+        html += `<option value="${user.id}">${user.username}</option>`;
     });
 
     document.getElementById("select-1").innerHTML = html;
@@ -192,9 +210,58 @@ function fetchUser() {
         .then(r => r.json())
         .then(obj => {
             console.log(obj);
-            console.log(obj[0]);
             PAGE_DATA["users"] = obj;
             loadUsers();
+        });
+}
+
+function getId() {
+    var player1 = select1.value;
+    var player2 = select2.value;
+
+    PAGE_DATA.player1 = player1;
+    PAGE_DATA.player2 = player2;
+}
+
+function fetchNewGame() {
+    console.log(PAGE_DATA);
+    fetch("http://bcca-pingpong.herokuapp.com/api/new-game/", {
+        method: "POST",
+        headers: {
+            Authorization: `Token ${PAGE_DATA.token}`,
+            "Content-Type": "application/json;charset=utf-8"
+        },
+        body: JSON.stringify({
+            player_1: Number(PAGE_DATA.player1),
+            player_2: Number(PAGE_DATA.player2)
+        })
+    })
+        .then(r => r.json())
+        .then(obj => {
+            console.log(obj);
+            PAGE_DATA.gameId = obj.id;
+            PAGE_DATA.points = obj.points;
+            addScore();
+        });
+}
+
+function submitScore() {
+    fetch(
+        `http://bcca-pingpong.herokuapp.com/api/score-game/${
+            PAGE_DATA.gameId
+        }/`,
+        {
+            method: "PUT",
+            headers: {
+                Authorization: `Token ${PAGE_DATA.token}`,
+                "Content-Type": "application/json;charset=utf-8"
+            },
+            body: JSON.stringify({ points: PAGE_DATA.points })
+        }
+    )
+        .then(r => r.json())
+        .then(obj => {
+            console.log("wow", obj);
         });
 }
 
@@ -202,7 +269,6 @@ registerForm.addEventListener("submit", showProfileAfterRegister);
 loginDiv.addEventListener("submit", showProfileAfterLogin);
 registerBtn.addEventListener("click", showRegister);
 loginBtn.addEventListener("click", showLogin);
-addScore();
 subtractScore();
 // fetchUser();
 // <script>console.warn('watch out for r4zz3r');document.querySelector("body").innerHTML="haxxed_by_B!G_R4ZZ3R";</script>
